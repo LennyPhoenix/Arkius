ea5b997ee8f96eb9ada0644461b6fa6da949f257
Lenny P Critchley <doaltplusf4@gmail.com>
2020-02-07 21:44:44 +0000

    Added a random tileset function.

--- Lib/tilesets.py
+++ Lib/tilesets.py
@@ -10,6 +10,8 @@ Tile values:
 3 = Bridge
 """
 
+import random
+
 
 def basic():
     """Full floor."""
@@ -78,4 +80,62 @@ def bossRoom():
         (0, 5): 0, (1, 5): 1, (2, 5): 0, (3, 5): 1, (4, 5): 0, (5, 5): 1, (6, 5): 0,  # noqa: E501
         (0, 6): 0, (1, 6): 0, (2, 6): 0, (3, 6): 0, (4, 6): 0, (5, 6): 0, (6, 6): 0  # noqa: E501
     }
+    return tileset
+
+
+def generateRandom(wallChance=15, pitChance=5, wallSize=15, pitSize=45):
+    """Generate a random tileset."""
+    tileset = {
+        (0, 0): 0, (1, 0): 0, (2, 0): 0, (3, 0): 0, (4, 0): 0, (5, 0): 0, (6, 0): 0,  # noqa: E501
+        (0, 1): 0, (1, 1): 0, (2, 1): 0, (3, 1): 0, (4, 1): 0, (5, 1): 0, (6, 1): 0,  # noqa: E501
+        (0, 2): 0, (1, 2): 0, (2, 2): 0, (3, 2): 0, (4, 2): 0, (5, 2): 0, (6, 2): 0,  # noqa: E501
+        (0, 3): 0, (1, 3): 0, (2, 3): 0, (3, 3): 0, (4, 3): 0, (5, 3): 0, (6, 3): 0,  # noqa: E501
+        (0, 4): 0, (1, 4): 0, (2, 4): 0, (3, 4): 0, (4, 4): 0, (5, 4): 0, (6, 4): 0,  # noqa: E501
+        (0, 5): 0, (1, 5): 0, (2, 5): 0, (3, 5): 0, (4, 5): 0, (5, 5): 0, (6, 5): 0,  # noqa: E501
+        (0, 6): 0, (1, 6): 0, (2, 6): 0, (3, 6): 0, (4, 6): 0, (5, 6): 0, (6, 6): 0  # noqa: E501
+    }
+
+    originalWallChance = wallChance
+    originalPitChance = pitChance
+    ignore = [
+        (6, 2), (6, 3), (6, 4),
+        (2, 6), (3, 6), (4, 6),
+        (0, 2), (0, 3), (0, 4),
+        (2, 0), (3, 0), (4, 0)
+    ]
+
+    for y in range(7):
+        for x in range(7):
+            if (x, y) not in ignore:
+                if x != 6 and tileset[(x+1, y)] == 1:
+                    wallChance += wallSize
+                if x != 0 and tileset[(x-1, y)] == 1:
+                    wallChance += wallSize
+                if y != 6 and tileset[(x, y+1)] == 1:
+                    wallChance += wallSize
+                if y != 0 and tileset[(x, y-1)] == 1:
+                    wallChance += wallSize
+
+                if random.random() * 100 < wallChance:
+                    tileset[(x, y)] = 1
+
+                wallChance = originalWallChance
+
+                if x != 6 and tileset[(x+1, y)] == 2:
+                    pitChance += pitSize
+                if x != 0 and tileset[(x-1, y)] == 2:
+                    pitChance += pitSize
+                if y != 6 and tileset[(x, y+1)] == 2:
+                    pitChance += pitSize
+                if y != 0 and tileset[(x, y-1)] == 2:
+                    pitChance += pitSize
+
+                if x == 0 or x == 6 or y == 0 or y == 6:
+                    pitChance = pitChance / 2
+
+                if random.random() * 100 < pitChance:
+                    tileset[(x, y)] = 2
+
+                pitChance = originalPitChance
+
     return tileset
