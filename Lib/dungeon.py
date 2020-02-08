59964bc74ef028883229bffce5ad92e7923b7491
Lenny P Critchley <doaltplusf4@gmail.com>
2020-02-08 11:02:40 +0000

    Moved class arguments docstring to main docstring.

--- Lib/dungeon.py
+++ Lib/dungeon.py
@@ -4,32 +4,33 @@ from . import tilesets
 
 
 class Room(object):
-    """Room class for dungeon."""
+    """
+    Room class for dungeon.
+
+    Args:
+        Type: (int)
+        Selects the room type. Versions include:
+        0 = Start
+        1 = Fight
+        2 = Treasure
+        3 = Boss
+        4 = Shop
+        Defaults to Start.
+
+        Pos: (Tuple (X, Y))
+        The room's position in the dungeon.
+        Defaults to (0, 0).
+
+        Doors: (Dict {DoorID: True/False})
+        The open doors of the room.
+        Defaults to the top door only.
+
+        Tileset: (Dict {(X, Y): TileValue})
+        The tiles that make up the room's ground.
+    """
 
     def __init__(self, roomType=0, pos=(0, 0), doors={0: True, 1: False, 2: False, 3: False}, tileset=tilesets.basic()):  # noqa: E501
-        """
-        Initialise the Room class.
-
-        Args:
-            Type: (int)
-            Selects the room type. Versions include:
-            0 = Start
-            1 = Fight
-            2 = Treasure
-            3 = Boss
-            4 = Shop
-            Defaults to Start.
-
-            Pos: (Tuple (X, Y))
-            The room's position in the dungeon.
-            Defaults to (0, 0).
-
-            Doors: (Dict {DoorID: True/False})
-            The open doors of the room.
-            Defaults to the top door only.
-
-            Tileset: (Dict {(X, Y): TileValue})
-        """
+        """Initialise the Room class."""
         self.type = roomType
         self.pos = pos
         self.doors = doors
