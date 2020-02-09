1070d254f6e213fe6e53e0b67354b81030c2b64b
Lenny P Critchley <doaltplusf4@gmail.com>
2020-02-09 21:23:47 +0000

    Updated GUI to use the 3 basic tiles.

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import pyglet
 from pyglet.window import Window
+from pyglet.window import key
+from pyglet import image
 
 from Lib.dungeon import Room
 from Lib import tilesets
@@ -31,85 +33,101 @@ def toString(tileset):
     return string
 
 
+def getValue(tileset, x, y):
+    """Returns the bitmasking value of a tile."""
+    tileID = tileset[(x, y)]
+    value = 255
+    sides = {
+        128: False, 1: False,   2: False,
+        64: False,              4: False,
+        32: False,  16: False,  8: False
+    }
+
+    if y != 0 and tileset[(x, y-1)] != tileID:
+        sides[128] = True
+        sides[1] = True
+        sides[2] = True
+    if y != 0 and x != 14 and tileset[(x+1, y-1)] != tileID:
+        sides[2] = True
+    if x != 14 and tileset[(x+1, y)] != tileID:
+        sides[2] = True
+        sides[4] = True
+        sides[8] = True
+    if x != 14 and y != 14 and tileset[(x+1, y+1)] != tileID:
+        sides[8] = True
+    if y != 14 and tileset[(x, y+1)] != tileID:
+        sides[8] = True
+        sides[16] = True
+        sides[32] = True
+    if y != 14 and x != 0 and tileset[(x-1, y+1)] != tileID:
+        sides[32] = True
+    if x != 0 and tileset[(x-1, y)] != tileID:
+        sides[32] = True
+        sides[64] = True
+        sides[128] = True
+    if x != 0 and y != 0 and tileset[(x-1, y-1)] != tileID:
+        sides[128] = True
+
+    for side in sides.keys():
+        if sides[side]:
+            value -= side
+
+    return value
+
+
 room = Room(roomType=1, tileset=tilesets.generateRandom())
-string = toString(room.groundTiles)
+print(toString(room.groundTiles))
+
+print("\n\n\n")
 
 window = Window(caption="Arkius", resizable=True)
-tiles = pyglet.text.Label(
-    string,
-    font_name="Times New Roman",
-    font_size=10,
-    x=window.width/2,
-    y=window.height/2,
-    anchor_x="center",
-    anchor_y="center",
-    multiline=True,
-    width=162
-)
+tileBatch = pyglet.graphics.Batch()
 
-helptext = pyglet.text.Label(
-    "Left click to generate random room, right click to generate boss room.",
-    font_name="Times New Roman",
-    font_size=20,
-    x=window.width/2, y=window.height - 40,
-    anchor_x="center",
-    anchor_y="center",
-    multiline=True,
-    width=400
-)
+
+@window.event
+def on_key_press(symbol, modifiers):
+    global room
+    # if symbol == key._0:
+    #     room = Room(roomType=0, tileset=tilesets.generateRandom())
+    if symbol == key._1:
+        room = Room(roomType=1, tileset=tilesets.generateRandom())
+    # elif symbol == key._2:
+    #     room = Room(roomType=2, tileset=tilesets.generateRandom())
+    # elif symbol == key._3:
+    #     room = Room(roomType=3, tileset=tilesets.generateRandom())
+    elif symbol == key._2:
+        room = Room(roomType=4, tileset=tilesets.generateRandom())
 
 
 @window.event
 def on_draw():
-    window.clear()
-    tiles.draw()
-    helptext.draw()
+    tiles = []
 
+    for x in range(15):
+        for y in range(15):
+            roomTiles = room.groundTiles
+            tileID = roomTiles[(x, y)]
 
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    global tiles, helptext
-    if button == 1:
-        room = Room(roomType=1, tileset=tilesets.generateRandom())
-        string = toString(room.groundTiles)
-        tiles = pyglet.text.Label(
-            string,
-            font_name="Times New Roman",
-            font_size=10,
-            x=window.width/2,
-            y=window.height/2,
-            anchor_x="center",
-            anchor_y="center",
-            multiline=True,
-            width=162
-        )
-    elif button == 4:
-        room = Room(roomType=3)
-        string = toString(room.groundTiles)
-        tiles = pyglet.text.Label(
-            string,
-            font_name="Times New Roman",
-            font_size=10,
-            x=window.width/2,
-            y=window.height/2,
-            anchor_x="center",
-            anchor_y="center",
-            multiline=True,
-            width=162
-        )
+            if tileID != 0:
+                value = getValue(roomTiles, x, y)
+                tileImage = image.load(f"Images/Tiles/1-{tileID}/{value}.png")
+                tiles.append(tileImage)
+            else:
+                tileImage = image.load(f"Images/Tiles/1-0.png")
+            tileImage.anchor_x = 0
+            tileImage.anchor_y = tileImage.height
+            tile = pyglet.sprite.Sprite(
+                tileImage,
+                batch=tileBatch,
+                x=x*16*window.height/240 +
+                (window.width/2-120*window.height/240),
+                y=window.height-y*16*window.height/240
+            )
+            tile.scale = window.height/240
+            tiles.append(tile)
+
     window.clear()
-    tiles.draw()
-    helptext = pyglet.text.Label(
-        "Left click to generate random room, right click to generate boss room.",
-        font_name="Times New Roman",
-        font_size=20,
-        x=window.width/2, y=window.height - 40,
-        anchor_x="center",
-        anchor_y="center",
-        multiline=True,
-        width=400
-    )
-    helptext.draw()
+    tileBatch.draw()
 
 
 pyglet.app.run()
