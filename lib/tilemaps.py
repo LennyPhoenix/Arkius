2cbd09f55ab7a4ec5edfc8cb3d607d3440f37ab9
Lenny P Critchley <doaltplusf4@gmail.com>
2020-03-27 19:40:55 +0000

    Added type argument to create_blank.

--- lib/tilemaps.py
+++ lib/tilemaps.py
@@ -1,12 +1,13 @@
 """Contains tilemap generator and premade tilemaps for rooms."""
 
 
-def create_blank(width=7, height=7):
+def create_blank(width=7, height=7, type=0):
     """Creates a coordinate based dictionary with the specified width and height.
 
     Keyword Arguments:
         width {int} -- The room's width. (default: {7})
         height {int} -- The room's height. (default: {7})
+        type {int} -- The tile type that should be used. (default: {0})
 
     Returns:
         dict -- The empty map.
@@ -14,5 +15,5 @@ def create_blank(width=7, height=7):
     map = {}
     for x in range(-width, width+1):
         for y in range(-height, height+1):
-            map[(x, y)] = 0
+            map[(x, y)] = type
     return map
