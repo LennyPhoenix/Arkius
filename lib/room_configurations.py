815a9f3ed4458164de59451e94efccc17c09a7fd
Lenny P Critchley <doaltplusf4@gmail.com>
2020-04-18 17:31:05 +0100

    Added templates for room config.

--- lib/room_configurations.py
+++ lib/room_configurations.py
@@ -1,2 +1,34 @@
 """Contains room configurations."""
 
+GENERATION_OPTIONS = {
+    "id": int,
+    "overrides": [int, int],
+    "seed_amount": int,
+    "seed_type": "blob/line",
+    "b_spread_amount": int,
+    "b_spread_additional": int,
+    "b_spread_compound": bool,
+    "l_hole_amount": int,
+    "l_hole_size_range": (int, int)
+}
+
+MAP = {
+    "width": int,
+    "height": int,
+    "door_info": {
+        0: {"pos": int, "floor": int},
+        1: {"pos": int, "floor": int},
+        2: {"pos": int, "floor": int},
+        3: {"pos": int, "floor": int},
+    },
+    "matrix": [
+        [int, int, int],
+        [int, int, int],
+        [int, int, int]
+    ]
+}
+
+CONFIG = {
+    "options": GENERATION_OPTIONS,
+    "maps": [MAP, MAP]
+}
