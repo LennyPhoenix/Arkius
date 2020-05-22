4e2c4559d8282bbb0a0f35aacf0d1f963e5de2bd
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-22 15:11:00 +0100

    Updated door colliders.

--- source/room.py
+++ source/room.py
@@ -58,26 +58,50 @@ class Room:
         self.border_colliders = {
             "top": pymunk.Segment(
                 self.space.static_body,
-                (-(self.width+3)*c.TILE_SIZE, (self.height+4)*c.TILE_SIZE),
-                ((self.width+4)*c.TILE_SIZE, (self.height+4)*c.TILE_SIZE),
+                (
+                    -(self.width+3.5)*c.TILE_SIZE,
+                    (self.height+4.5)*c.TILE_SIZE
+                ),
+                (
+                    (self.width+4.5)*c.TILE_SIZE,
+                    (self.height+4.5)*c.TILE_SIZE
+                ),
                 1
             ),
             "right": pymunk.Segment(
                 self.space.static_body,
-                ((self.width+4)*c.TILE_SIZE, -(self.height+3)*c.TILE_SIZE),
-                ((self.width+4)*c.TILE_SIZE, (self.height+4)*c.TILE_SIZE),
+                (
+                    (self.width+4.5)*c.TILE_SIZE,
+                    -(self.height+3.5)*c.TILE_SIZE
+                ),
+                (
+                    (self.width+4.5)*c.TILE_SIZE,
+                    (self.height+4.5)*c.TILE_SIZE
+                ),
                 1
             ),
             "bottom": pymunk.Segment(
                 self.space.static_body,
-                (-(self.width+3)*c.TILE_SIZE, -(self.height+3)*c.TILE_SIZE),
-                ((self.width+4)*c.TILE_SIZE, -(self.height+3)*c.TILE_SIZE),
+                (
+                    -(self.width+3.5)*c.TILE_SIZE,
+                    -(self.height+3.5)*c.TILE_SIZE
+                ),
+                (
+                    (self.width+4.5)*c.TILE_SIZE,
+                    -(self.height+3.5)*c.TILE_SIZE
+                ),
                 1
             ),
             "left": pymunk.Segment(
                 self.space.static_body,
-                (-(self.width+3)*c.TILE_SIZE, -(self.height+3)*c.TILE_SIZE),
-                (-(self.width+3)*c.TILE_SIZE, (self.height+4)*c.TILE_SIZE),
+                (
+                    -(self.width+3.5)*c.TILE_SIZE,
+                    -(self.height+3.5)*c.TILE_SIZE
+                ),
+                (
+                    -(self.width+3.5)*c.TILE_SIZE,
+                    (self.height+4.5)*c.TILE_SIZE
+                ),
                 1
             )
         }
